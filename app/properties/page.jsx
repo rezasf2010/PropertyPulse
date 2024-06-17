c933e92595df6e39c83c8f7402fab63a8ce6d51a
Reza Saberifard <rezasf2010@gmail.com>
2024-06-17 18:15:55 +0330

    End of Sec 10 Part 2

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,11 +1,7 @@
-import PorpertyCard from "@/components/PorpertyCard";
-import { fetchProperties } from "@/utils/requests";
+import Properties from "@/components/Properties";
 import ProppertySearchFrom from "@/components/ProppertySearchFrom";
 
 const PropertiesPage = async () => {
-  const properties = await fetchProperties();
-  properties.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
-
   return (
     <>
       <section className="bg-blue-700 py-4">
@@ -13,20 +9,7 @@ const PropertiesPage = async () => {
           <ProppertySearchFrom />
         </div>
       </section>
-
-      <section className="px-4 py-6">
-        <div className="container-xl lg:container m-auto px-4 py-6">
-          {properties.length === 0 ? (
-            <p>No properties found</p>
-          ) : (
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-              {properties.map((property) => (
-                <PorpertyCard key={property._id} property={property} />
-              ))}
-            </div>
-          )}
-        </div>
-      </section>
+      <Properties />
     </>
   );
 };
