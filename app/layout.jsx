ee1745dd3057ebc652c82750d10cea411e562a5c
Reza Saberifard <rezasf2010@gmail.com>
2024-06-17 17:35:40 +0330

    first commit after endign section 9

---
+++ app/layout.jsx
@@ -0,0 +1,32 @@
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import AuthProvider from "@/components/AuthProvider";
+import { ToastContainer } from "react-toastify";
+import { GlobalProvider } from "@/context/GlobalContext";
+import "@/assets/styles/globals.css";
+import "react-toastify/dist/ReactToastify.css";
+
+export const metadata = {
+  title: "property",
+  discription: "find your dream rental property",
+  keywords: "rental, find rental, find property",
+};
+
+const MainLayout = ({ children }) => {
+  return (
+    <GlobalProvider>
+      <AuthProvider>
+        <html lang="en">
+          <body>
+            <Navbar />
+            <main>{children}</main>
+            <Footer />
+            <ToastContainer />
+          </body>
+        </html>
+      </AuthProvider>
+    </GlobalProvider>
+  );
+};
+
+export default MainLayout;
---
+++ app/properties/page.jsx
@@ -0,0 +1,34 @@
+import PorpertyCard from "@/components/PorpertyCard";
+import { fetchProperties } from "@/utils/requests";
+import ProppertySearchFrom from "@/components/ProppertySearchFrom";
+
+const PropertiesPage = async () => {
+  const properties = await fetchProperties();
+  properties.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
+
+  return (
+    <>
+      <section className="bg-blue-700 py-4">
+        <div className="max-w-7xl mx-auto px-4 flex flex-col items-start sm:px-6 lg:px-8">
+          <ProppertySearchFrom />
+        </div>
+      </section>
+
+      <section className="px-4 py-6">
+        <div className="container-xl lg:container m-auto px-4 py-6">
+          {properties.length === 0 ? (
+            <p>No properties found</p>
+          ) : (
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+              {properties.map((property) => (
+                <PorpertyCard key={property._id} property={property} />
+              ))}
+            </div>
+          )}
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default PropertiesPage;
